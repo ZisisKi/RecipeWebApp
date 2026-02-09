46f87149238d1c1ea3d54c6850b383f88a791241
alex_Palladis <alehandro4443@gmail.com>
2026-02-09 18:27:03 +0200

    last

--- src/App.js
+++ src/App.js
@@ -80,14 +80,14 @@ function App() {
                 Μια εφαρμογή Συνταγών
                 <br /> για όλες τις Κουζίνες
               </h2>
-             
+
               <p className={classes.heroSubHeadline}>
-               <span className={classes.heroWord}>Δημιούργησε</span>
-               <span className={classes.heroDot}>•</span>
-               <span className={classes.heroWord}>Αποθήκευσε</span>
-               <span className={classes.heroDot}>•</span>
-               <span className={classes.heroWord}>Εκτέλεσε</span>
-             </p>
+                <span className={classes.heroWord}>Δημιούργησε</span>
+                <span className={classes.heroDot}>•</span>
+                <span className={classes.heroWord}>Αποθήκευσε</span>
+                <span className={classes.heroDot}>•</span>
+                <span className={classes.heroWord}>Εκτέλεσε</span>
+              </p>
 
               <nav className={classes.mainMenu}>
                 <button
@@ -165,7 +165,7 @@ function App() {
           <span>© 2026 Recipe eBook Project</span>
           <span className={classes.footerSeparator}>|</span>
           <span className={classes.footerTeam}>
-            Powered by: <strong>Alex / Zisis / Stelios / Argiris</strong>
+            Powered by: <strong>Alex Pal</strong>
           </span>
         </div>
       </footer>
